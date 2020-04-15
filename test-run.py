3dd62e247e2b59b1b3d9258b7e4cd2331d5aed7a
Valentin Yakovlev <valentin.yakovlev@namecheap.com>
2020-04-15 11:54:07 +0300

    init

---
+++ test-run.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.keys import Keys
+import pytest
+
+
+@pytest.fixture
+def chrome_options(chrome_options, pytestconfig):
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--no-sandbox')
+    return chrome_options
+
+
+def test_page_title(selenium):
+    selenium.implicitly_wait(15)
+    selenium.maximize_window()
+    selenium.get('https://2ip.ru')
+    assert 'VPN' in selenium.page_source
